4f6386aa93352fc612d638f9c54aa498c85d983b
vishalkumarsingh015 <vishalpratapsingh143047@gmail.com>
2023-10-20 05:08:34 +0530

    Ctratingformwithtaghelper
    source code Ctratingformwithtaghelper

---
+++ CreatingFormwithTaghelper/CreatingFormwithTaghelper/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace CreatingFormwithTaghelper.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public int age{ get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+        public string Married { get; set; }
+        public string Description { get; set; }
+
+    }
+    public enum Gender
+    {
+        MALE,FEMALE
+    }
+}
